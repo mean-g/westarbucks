bc6297152c489e17cffd1c7b2eb4b5b1e48018d4
mean-g <106012542+mean-g@users.noreply.github.com>
2022-07-02 20:17:06 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'w2w',
+        'USER': 'root',
+        'PASSWORD': '9893jbmj?!!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+
+
+SECRET_KEY = 'django-insecure-z2@1g6+9b!ooy2_*n4^5qe_xw2#=o$(*7zbl$xm&uw6vo5wxp^'
